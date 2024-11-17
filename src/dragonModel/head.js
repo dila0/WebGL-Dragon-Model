146ecdddff33ca088fdd7f49e65b0eda8cffcf91
Dila Tosun <dilatosun@gmail.com>
2024-11-17 19:31:07 +0300

    jaw, head implementation for hierarchy

--- src/dragonModel/head.js
+++ src/dragonModel/head.js
@@ -9,38 +9,71 @@
  */
 
 // Global variables
-var headHeight = 0.5;
+var headHeight = 1.0;
 var headWidth = 0.5;
 
 // Function to initialize the left head
 function initLeftHead(){
     var m = mat4();
+    m = translate(0.0, jawHeight + 0.5 * headHeight, 0.0);
+    m = mult(m, rotate(theta[LEFT_HEAD_ID], 0, 1, 0));
+    m = mult(m, translate(0.0, -0.5 * headHeight, 0.0));
+
     figure[LEFT_HEAD_ID] = createNode(m, renderLeftHead, null, null);
 }
 
 // Function to initialize the middle head
 function initMidHead(){
     var m = mat4();
+    m = translate(0.0, jawHeight + 0.5 * headHeight, 0.0);
+    m = mult(m, rotate(theta[MID_HEAD_ID], 0, 1, 0));
+    m = mult(m, translate(0.0, -0.5 * headHeight, 0.0));
+
     figure[MID_HEAD_ID] = createNode(m, renderMidHead, null, null);
 }
 
 // Function to initialize the right head
 function initRightHead(){
     var m = mat4();
+    m = translate(0.0, jawHeight + 0.5 * headHeight, 0.0);
+    m = mult(m, rotate(theta[RIGHT_HEAD_ID], 0, 1, 0));
+    m = mult(m, translate(0.0, -0.5 * headHeight, 0.0));
+
     figure[RIGHT_HEAD_ID] = createNode(m, renderRightHead, null, null);
 }
 
 // Function to render the left head
 function renderLeftHead(){
+    instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5 * headHeight, 0.0));
+    instanceMatrix = mult(instanceMatrix, scale4(headWidth, headHeight, headWidth));
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
 
+    gl.uniform4fv(gl.getUniformLocation(program, "uColor"), flatten(vec4(1.0, 0.796, 0.0, 1.0)));
+    for(var i = 0; i < 6; i++){
+        gl.drawArrays(gl.TRIANGLE_FAN, 4*i, 4);
+    }
 }
 
 // Function to render the middle head
 function renderMidHead(){
+    instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5 * headHeight, 0.0));
+    instanceMatrix = mult(instanceMatrix, scale4(headWidth, headHeight, headWidth));
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
 
+    gl.uniform4fv(gl.getUniformLocation(program, "uColor"), flatten(vec4(1.0, 0.796, 0.0, 1.0)));
+    for(var i = 0; i < 6; i++){
+        gl.drawArrays(gl.TRIANGLE_FAN, 4*i, 4);
+    }
 }
 
 // Function to render the right head
 function renderRightHead(){
+    instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5 * headHeight, 0.0));
+    instanceMatrix = mult(instanceMatrix, scale4(headWidth, headHeight, headWidth));
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
 
+    gl.uniform4fv(gl.getUniformLocation(program, "uColor"), flatten(vec4(1.0, 0.796, 0.0, 1.0)));
+    for(var i = 0; i < 6; i++){
+        gl.drawArrays(gl.TRIANGLE_FAN, 4*i, 4);
+    }
 }
--- src/dragonModel/jaw.js
+++ src/dragonModel/jaw.js
@@ -15,32 +15,65 @@ var jawWidth = 0.5;
 // Function to initialize the left jaw
 function initLeftJaw(){
     var m = mat4();
+    m = translate(0.0, upperNeckHeight + 0.5 * jawHeight, 0.0);
+    m = mult(m, rotate(theta[LEFT_JAW_ID], 0, 1, 0));
+    m = mult(m, translate(0.0, -0.5 * jawHeight, 0.0));
+    
     figure[LEFT_JAW_ID] = createNode(m, renderLeftJaw, null, LEFT_HEAD_ID);
 }
 
 // Function to initialize the middle jaw
 function initMidJaw(){
     var m = mat4();
+    m = translate(0.0, upperNeckHeight + 0.5 * jawHeight, 0.0);
+    m = mult(m, rotate(theta[MID_JAW_ID], 0, 1, 0));
+    m = mult(m, translate(0.0, -0.5 * jawHeight, 0.0));
+
     figure[MID_JAW_ID] = createNode(m, renderMidJaw, null, MID_HEAD_ID);
 }   
 
 // Function to initialize the right jaw
 function initRightJaw(){
     var m = mat4();
+    m = translate(0.0, upperNeckHeight + 0.5 * jawHeight, 0.0);
+    m = mult(m, rotate(theta[RIGHT_JAW_ID], 0, 1, 0));
+    m = mult(m, translate(0.0, -0.5 * jawHeight, 0.0));
+
     figure[RIGHT_JAW_ID] = createNode(m, renderRightJaw, null, RIGHT_HEAD_ID);
 }
 
 // Function to render the left jaw
 function renderLeftJaw(){
+    instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5 * jawHeight, 0.0));
+    instanceMatrix = mult(instanceMatrix, scale4(jawWidth, jawHeight, jawWidth));
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
 
+    gl.uniform4fv(gl.getUniformLocation(program, "uColor"), flatten(vec4(1.0, 0.424, 0.0, 1.0)));
+    for(var i = 0; i < 6; i++){
+        gl.drawArrays(gl.TRIANGLE_FAN, 4*i, 4);
+    }
 }
 
 // Function to render the middle jaw
 function renderMidJaw(){
+    instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5 * jawHeight, 0.0));
+    instanceMatrix = mult(instanceMatrix, scale4(jawWidth, jawHeight, jawWidth));
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
 
+    gl.uniform4fv(gl.getUniformLocation(program, "uColor"), flatten(vec4(1.0, 0.424, 0.0, 1.0)));
+    for(var i = 0; i < 6; i++){
+        gl.drawArrays(gl.TRIANGLE_FAN, 4*i, 4);
+    }
 }
 
 // Function to render the right jaw
 function renderRightJaw(){
+    instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5 * jawHeight, 0.0));
+    instanceMatrix = mult(instanceMatrix, scale4(jawWidth, jawHeight, jawWidth));
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
 
+    gl.uniform4fv(gl.getUniformLocation(program, "uColor"), flatten(vec4(1.0, 0.424, 0.0, 1.0)));
+    for(var i = 0; i < 6; i++){
+        gl.drawArrays(gl.TRIANGLE_FAN, 4*i, 4);
+    }
 }
