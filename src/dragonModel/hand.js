6277a9c6140aaa9545a6daf422d9b52886c2e675
Dila Tosun <dilatosun@gmail.com>
2024-11-25 15:52:12 +0300

    right hand implemented.

--- src/dragonModel/hand.js
+++ src/dragonModel/hand.js
@@ -28,7 +28,8 @@ function initRightHand() {
     var m = mat4();
     m = translate(0.5 * lowerWingHeight, 0.0, 0.0);
     m = rotatePart(RIGHT_HAND_ID, m);
-    m = mult(m, translate(0.5 * handWidth, 0.0, 0.0));
+    m = mult(m, rotate(120, 0, 0, 1));
+    m = mult(m, translate(-0.2 * handHeight, - 0.5 * handWidth, 0.0));
 
     figure[RIGHT_HAND_ID] = createNode(m, renderRightHand, null, null);
 }
@@ -77,6 +78,34 @@ function renderRightHand() {
     instanceMatrix = mult(instanceMatrix, scale4(handWidth, handHeight, handHeight));
     gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
 
-    gl.uniform4fv(gl.getUniformLocation(program, "uColor"), flatten(vec4(1.0, 0.0, 0.0, 1.0)));
-    drawCube();
+    gl.uniform4fv(gl.getUniformLocation(program, "uColor"), flatten(vec4(0.463, 0.208, 0.251, 1.0)));
+    drawSphere();
+
+    const coneScale = vec3(0.15, 0.4, 0.2); 
+
+    let finger1Matrix = mult(modelViewMatrix, translate(0.0, -0.5 * handHeight, 0.0)); 
+    finger1Matrix = mult(finger1Matrix, rotate(180, 0, 0, 1));
+    finger1Matrix = mult(finger1Matrix, scale4(coneScale[0], coneScale[1], coneScale[2])); 
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(finger1Matrix));
+
+    gl.uniform4fv(gl.getUniformLocation(program, "uColor"), flatten(vec4(0.631, 0.608, 0.596, 1.0))); 
+    drawCube(0.5);
+
+    let finger2Matrix = mult(modelViewMatrix, translate(-0.5 * handWidth, -0.5 * handHeight, 0.0)); 
+    finger2Matrix = mult(finger2Matrix, rotate(-45, 0, 0, 1));
+    finger2Matrix = mult(finger2Matrix, rotate(180, 0, 0, 1));
+    finger2Matrix = mult(finger2Matrix, scale4(coneScale[0], coneScale[1], coneScale[2])); 
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(finger2Matrix));
+
+    gl.uniform4fv(gl.getUniformLocation(program, "uColor"), flatten(vec4(0.631, 0.608, 0.596, 1.0)));
+    drawCube(0.5);
+
+    let finger3Matrix = mult(modelViewMatrix, translate(0.5 * handWidth, -0.5 * handHeight, 0.0)); 
+    finger3Matrix = mult(finger3Matrix, rotate(45, 0, 0, 1));
+    finger3Matrix = mult(finger3Matrix, rotate(180, 0, 0, 1));
+    finger3Matrix = mult(finger3Matrix, scale4(coneScale[0], coneScale[1], coneScale[2])); 
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(finger3Matrix));
+
+    gl.uniform4fv(gl.getUniformLocation(program, "uColor"), flatten(vec4(0.631, 0.608, 0.596, 1.0))); 
+    drawCube(0.5);
 }
